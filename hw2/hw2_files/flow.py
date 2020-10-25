fcf92d09fcf2f4228c2f6801128addc462ab72f7
amojena <am3238@cornell.edu>
2020-10-25 17:31:03 -0400

    init commit hw2

---
+++ hw2/hw2_files/flow.py
@@ -0,0 +1,19 @@
+# include any code you need for your assignment in this file or in auxiliary
+# files that can be imported here.
+
+# 9 (a)
+# implement an algorithm that computes the maximum flow in a graph G
+# Note: you may represent the graph, source, sink, and edge capacities
+# however you want. You may also change the inputs to the function below.
+def max_flow(G, s, t, c):
+    return -1
+
+# 9 (c)
+# implement an algorithm that computes a maximum matching in a bipartite graph G
+# Note: you may represent the bipartite graph however you want
+def max_matching(G):
+    return M # a matching
+
+
+    
+
